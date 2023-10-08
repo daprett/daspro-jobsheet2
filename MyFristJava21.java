0309f3b04ef365d205239480e315256e86064f55
daprett <145349876+daprett@users.noreply.github.com>
2023-10-08 16:49:01 +0700

    Add files via upload

---
+++ MyFristJava21.java
@@ -0,0 +1,10 @@
+public class MyFristJava21{
+    public static void main(String[] args){
+        System.out.println("****IDENTITAS DIRI****");
+        System.out.println("Nama    : Muhammad Fairuz Daffa Athallah");
+        System.out.println("Absen   : 21");
+        System.out.println("Kelas   : 1C");
+        System.out.println("Prodi   : D-IV Sistem Informasi Bisnis");
+        System.out.println("Jurusan : Teknologi Informasi");
+    }
+}
